376947c829ca0797589743b647d5aa855748a3a7
Alexis <a.r.g.a9652@gmail.com>
2021-01-26 11:14:25 -0600

    Se agregan clases para la lógica del juego

---
+++ src/Tablero.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alexis
+ */
+public class Tablero {
+    int filas;
+    int columnas;
+    int matriz;
+    Coordenadas c;
+
+    public Tablero() {
+    }
+
+    public int getFilas() {
+        return filas;
+    }
+
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+
+    public int getColumnas() {
+        return columnas;
+    }
+
+    public void setColumnas(int columnas) {
+        this.columnas = columnas;
+    }
+
+    public int getMatriz() {
+        return matriz;
+    }
+
+    public void setMatriz(int matriz) {
+        this.matriz = matriz;
+    }
+    
+    
+    
+    
+}
